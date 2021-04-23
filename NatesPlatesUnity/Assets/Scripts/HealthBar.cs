796e31508b55cf7efdc50b2ddacabacab3915d88
LukeDuhe <Luke7490@gmail.com>
2021-04-22 21:05:53 -0500

    add ability to increase or decrease health with hard max and min

--- NatesPlatesUnity/Assets/Scripts/HealthBar.cs
+++ NatesPlatesUnity/Assets/Scripts/HealthBar.cs
@@ -4,24 +4,46 @@ using UnityEngine;
 
 public class HealthBar : MonoBehaviour
 {
-    public float maxHealth = 100;
-    public float hp = 100;
-    public float minHealth = 0;
+    private float maxHealth = 100;
+    private float hp = 100;
+    private float minHealth = 0;
 
     private Vector3 initialScale;
     private Vector3 initialPosition;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         initialScale = transform.localScale;
         initialPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.localScale = new Vector3(initialScale.x * (hp / maxHealth), initialScale.y, initialScale.z);
         transform.position = new Vector3(initialPosition.x + ((initialScale.x * .5f) * ((maxHealth - hp) / maxHealth)), initialPosition.y, initialPosition.z);
+    }
+
+    public void IncreaseHealth(float points)
+    {
+        if ((hp + points) > maxHealth)
+        {
+            hp = maxHealth;
+        } else
+        {
+            hp += points;
+        }
+    }
+
+    public void DecreaseHealth(float points)
+    {
+        if ((hp - points) < minHealth)
+        {
+            hp = minHealth;
+        }
+        else
+        {
+            hp -= points;
+        }
     }
 }
