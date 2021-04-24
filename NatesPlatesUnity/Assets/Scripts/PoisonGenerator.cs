6f9b8d14e3d91d54294c9fedef0a9fe9586900c6
Unknown <CourtlandCthulhu@gmail.com>
2021-04-24 14:46:01 -0500

    It doesn't work...but it will

--- NatesPlatesUnity/Assets/Scripts/PoisonGenerator.cs
+++ NatesPlatesUnity/Assets/Scripts/PoisonGenerator.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PoisonGenerator : MonoBehaviour
 {
+    public GameObject poisonProperty;
+    public GameObject gm;
+    // public GameMaster gm = gameObject.Find("GameMaster").GetComponent<GameMaster>();
+    void Start() {
+        gm = gameObject.Find("GameMaster") as GameObject;
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Collision with " + collision.gameObject.ToString());
         GameObject other = collision.gameObject;
         if(other.tag == "Tomato" || other.tag == "PlatedTomato" || other.tag == "Potato" || other.tag == "PlatedPotato")
         {
-            other.GetComponent<SpriteRenderer>().color = Color.green;
+            if(!other.transform.Find("PoisonProperty")) {
+                Debug.Log("Applying Poison");
+                other.GetComponent<SpriteRenderer>().color = Color.green;
+                GameObject prop = Instantiate(poisonProperty, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+                prop.GetComponent<Transform>().parent = other.transform;
+                // gm.TakeAction();
+            }
+
         }
     }
 }
