a028f5e9ebdd6242edf384759ed9b8e1536f1821
LukeDuhe <Luke7490@gmail.com>
2021-04-24 13:59:09 -0500

    add moving recepticals prefabs

---
+++ NatesPlatesUnity/Assets/Scripts/RecepticalMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecepticalMovement : MonoBehaviour
+{
+    public bool movementActive = false;
+
+    public int speed = 1;
+
+    public int radius = 1;
+
+    public Vector3 centerPosition;
+
+    private bool movingRight;
+
+    private Vector3 xMax;
+    private Vector3 xMin;
+
+    private void Start()
+    {
+        xMin = new Vector3(centerPosition.x - radius, centerPosition.y, centerPosition.z);
+        xMax = new Vector3(centerPosition.x + radius, centerPosition.y, centerPosition.z);
+    }
+
+    private void Update()
+    {
+        if (movementActive)
+        {
+            if (Vector2.Distance(transform.position, xMax) < 0.01)
+            {
+                movingRight = false;
+            }
+            else if (Vector2.Distance(transform.position, xMin) < 0.01)
+            {
+                movingRight = true;
+            }
+
+            if (movingRight)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, xMax, speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, xMin, speed * Time.deltaTime);
+            } 
+        }
+    }
+}
