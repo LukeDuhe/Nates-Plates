80e2b64e98b6b4f7d65b3e95c030401c5e16cbaa
LukeDuhe <Luke7490@gmail.com>
2021-04-28 22:30:50 -0500

    add low health sequence and game over condition

--- NatesPlatesUnity/Assets/Scripts/HealthBar.cs
+++ NatesPlatesUnity/Assets/Scripts/HealthBar.cs
@@ -1,21 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     public float healthLossPerSecond = 1;
 
     private float maxHealth = 100;
-    private float hp = 100;
+    public float hp = 100;
     private float minHealth = 0;
 
     private Vector3 initialScale;
     private Vector3 initialPosition;
 
+    private Color startingColor;
+
+    private bool ded = false;
+
+    private bool gameOverTriggered = false;
+
+    public GameObject nate;
+    public Sprite irateNate;
+
+    public AudioClip roarSound;
+
+    public SpriteRenderer redDangerGlow;
+
+    public AudioSource musicPlayer;
+
+    public TentacleMover leftTentacle;
+    public TentacleMover rightTentacle;
+
+    public Text gameOverText;
 
     void Start()
     {
+        startingColor = gameObject.GetComponent<SpriteRenderer>().color;
+
         initialScale = transform.localScale;
         initialPosition = transform.position;
 
@@ -26,6 +48,51 @@ public class HealthBar : MonoBehaviour
     {
         transform.localScale = new Vector3(initialScale.x * (hp / maxHealth), initialScale.y, initialScale.z);
         transform.position = new Vector3(initialPosition.x + ((initialScale.x * .5f) * ((maxHealth - hp) / maxHealth)), initialPosition.y, initialPosition.z);
+
+        if (hp > 20)
+        {
+            if (!musicPlayer.isPlaying)
+            {
+                musicPlayer.Play();
+            }
+
+            gameObject.GetComponent<AudioSource>().Stop();
+            GetComponent<SpriteRenderer>().color = startingColor;
+            redDangerGlow.color = new Color(255, 0, 0, 0);
+        }
+        if (hp <= 20 && !ded)
+        {
+            if (musicPlayer.isPlaying)
+            {
+                musicPlayer.Stop();
+            }
+
+            GetComponent<SpriteRenderer>().color = Color.red;
+            redDangerGlow.color = new Color(255,0,0,.2f);
+
+            if (!gameObject.GetComponent<AudioSource>().isPlaying)
+            {
+                gameObject.GetComponent<AudioSource>().Play();
+            }
+        }
+        if (hp == 0)
+        {
+            ded = true;
+        }
+
+        if (!gameOverTriggered && ded)
+        {
+            gameOverText.enabled = true;
+            leftTentacle.enabled = false;
+            rightTentacle.enabled = false;
+            gameObject.GetComponent<AudioSource>().loop = false;
+            gameObject.GetComponent<AudioSource>().Stop();
+            gameObject.GetComponent<AudioSource>().volume = .6f;
+            gameObject.GetComponent<AudioSource>().PlayOneShot(roarSound);
+            gameOverTriggered = true;
+            nate.GetComponent<Animator>().enabled = false;
+            nate.GetComponent<SpriteRenderer>().sprite = irateNate;
+        }
     }
 
     private IEnumerator ConstantlyLoseHealth()
@@ -39,13 +106,16 @@ public class HealthBar : MonoBehaviour
 
     public void IncreaseHealth(float points)
     {
-        if ((hp + points) > maxHealth)
+        if (!ded)
         {
-            hp = maxHealth;
-        }
-        else
-        {
-            hp += points;
+            if ((hp + points) > maxHealth)
+            {
+                hp = maxHealth;
+            }
+            else
+            {
+                hp += points;
+            }
         }
     }
 
